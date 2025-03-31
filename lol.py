310f48864c3804a9fcba24a8e0c04c935ec7114e
Rsdpugeij creator <144550075+Rsdpugeij@users.noreply.github.com>
2025-03-31 16:33:24 +0200

    Update lol.py

--- lol.py
+++ lol.py
@@ -8,7 +8,7 @@ def draw_rects(dc, x, y, w, h, count, dx, dy):
     for i in range(count):
         brush = CreateSolidBrush(RGB(randrange(255),randrange(255),randrange(255)))
         SelectObject(dc, brush)
-        #PatBlt(dc, x + i * dx, y + i * dy, w - 2 * i * dx, h - 2 * i * dy, BLACKNESS)
+        PatBlt(dc, x + i * dx, y + i * dy, w - 2 * i * dx, h - 2 * i * dy, BLACKNESS)
         PatBlt(dc, x + i * dx, y + i * dy, w - 2 * i * dx, h - 2 * i * dy, PATINVERT)
 while True:
     draw_rects(dc, randrange(0,1920), randrange(0,1079), 250, 250, 120, 10, 10)
